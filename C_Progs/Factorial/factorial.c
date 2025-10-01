e2381322b17489310a9b4d59e84b6a59a6a2499d
birariparth <birariparth@gmail.com>
2025-10-02 01:10:01 +0530

    Factorial program completed

--- C_Progs/Factorial/factorial.c
+++ C_Progs/Factorial/factorial.c
@@ -2,7 +2,15 @@
 
 void main()
 {
-    printf ("Hello !!! Add logic for factorial ...");
+    int n, i;
+    printf("Enter number of which factorial is to be found ");
+    scanf("%d", &n);
+    int fact = 1;
+    for(i=1; i<=n ;i++)
+    {
+        fact *= i;
 
-    return;
+        printf("%d \n", fact);
+    }
+    printf("Factorial of number is: %d \n", fact);
 }
