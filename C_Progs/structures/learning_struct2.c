ed5fdbb457ac8d4d2664d5f627302436a487a468
birariparth <birariparth@gmail.com>
2025-10-02 16:21:49 +0530

    Added a new command to make copy of variable assigned to a structure

--- C_Progs/structures/learning_struct2.c
+++ C_Progs/structures/learning_struct2.c
@@ -11,9 +11,11 @@ int main()
 {
     struct stu s1 = {"Parth", 20, 99.9};
 
+    struct stu s1c = s1;        //creates a copy of s1
+
     struct stu s2 = {.age = 18, .name = "praneel", .grade = 82.3};
 
-    printf("%s \t %d \t %.2f \n", s1.name, s1.age, s1.grade);
+    printf("%s \t %d \t %.2f \n", s1c.name, s1c.age, s1c.grade);
     printf("%s \t %d \t %.2f \t", s2.name, s2.age, s2.grade);
     return 0;
 }
