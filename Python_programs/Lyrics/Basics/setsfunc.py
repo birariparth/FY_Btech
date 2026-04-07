f8c7d8c72cfe53c18153fcf9ad8577eedb987b6a
birariparth <birariparth@gmail.com>
2026-04-08 00:40:47 +0530

    s

---
+++ Python_programs/Lyrics/Basics/setsfunc.py
@@ -0,0 +1,10 @@
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+# i) Union
+print("Union:", set1 | set2)
+# ii) Intersection
+print("Intersection:", set1 & set2)
+# iii) Difference
+print("Difference (set1 - set2):", set1 - set2)
+# iv) Symmetric Difference
+print("Symmetric Difference:", set1 ^ set2)
