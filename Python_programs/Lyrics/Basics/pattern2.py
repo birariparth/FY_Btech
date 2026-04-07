ed48a8d6f75e5c14bdad3aab7eec8f3c07362fa7
birariparth <birariparth@gmail.com>
2026-04-07 22:27:49 +0530

    factorial py added

--- Python_programs/Lyrics/Basics/pattern2.py
+++ Python_programs/Lyrics/Basics/pattern2.py
@@ -1,5 +1,16 @@
 for i in range(1, 6):
-    for j in range(1, 6):
-        if j <= i:
-            print(i, end='')
+    for j in range(1, i + 1):
+        print(j, end='')
+    print("\n")
+
+
+text = "Python,Java,C++,C#,R "
+langs = text.split(",")
+print(langs)
+result = " | ".join(langs)
+print(result)
+
+for i in range(5, 0, -1):
+    for j in range(1, i + 1):
+        print(j, end='')
     print("\n")
